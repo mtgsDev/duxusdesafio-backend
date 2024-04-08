e22439df448e19f823eda40c2d27adde46505e6b
Matheus Gomes <7kiks7@gmail.com>
2024-04-08 12:04:38 -0300

    fix: ajuste '/IntegranteMaisUsado', add: criação de controller separado

--- src/main/java/br/com/duxusdesafio/controller/ComposicaoTimeController.java
+++ src/main/java/br/com/duxusdesafio/controller/ComposicaoTimeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/composicoes-time")
+@RequestMapping("/composicao-time")
 public class ComposicaoTimeController {
 
     private final ComposicaoTimeService composicaoTimeService;
